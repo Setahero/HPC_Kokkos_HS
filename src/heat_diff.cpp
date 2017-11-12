61c125d72320ba96891def90d6bc7599aaf508f2
Andrey <andrey.ruzhanskiy@mailbox.tu-dresden.de>
2017-11-12 15:17:27 +0100

    Added cml options, fixed Bound issues

--- src/heat_diff.cpp
+++ src/heat_diff.cpp
@@ -4,14 +4,19 @@
 #include <boost/program_options.hpp>
 
 
+//	Standard values, if not specified	//
+
 double dx = 1.0;
 double dy = 1.0;
-double k = 1.0;
-double dt = 0.05;
-int iteration = 5;
+double k = 1.0;		//Heattransfer Coefficient
+double dt = 0.05;	//Timestep
+int iteration = 5; 
+
 
 double array[10][10]={{0.0}};
 double array_new[10][10]={{0.0}};
+
+//	For printing purpose	//
 
 template< typename T, size_t N, size_t M >
 void printArray( T(&theArray)[N][M]  ) {
@@ -23,6 +28,26 @@ void printArray( T(&theArray)[N][M]  ) {
     }
 }
 
+//	Checking, if accesing out of bounds element	//
+
+int checkBounds(int i){
+	if (i<0){
+		return 9;
+	}else if (i==10)
+	{
+		return 0;
+	}
+}
+
+// 2D Stencil implementation	//
+
+double computeNewNode(double arr[][10], int i, int j){
+	double x = dx*dx;
+	double y = dy*dy;
+
+	return ((arr[checkBounds(i-1)][j] + arr[checkBounds(i+1)][j] - 2*arr[i][j])/
+		x) + ((arr[i][checkBounds(j-1)] + arr[i][checkBounds(j+1)] - 2*arr[i][j])/y);
+}
 
 
 
@@ -32,8 +57,10 @@ int main(int argc, char* argv[])
     namespace po = boost::program_options;
     po::options_description desc("Allowed options");
     desc.add_options()
-                ("help,h", "Print this Message")
+                ("options,o", "Print this Message")
                 ("iteration,i", po::value<int>(&iteration), "Set Iterations, standart 5")
+                ("timestep, t", po::value<double>(&dt),"Set timestep, standart 0.05")
+                ("heattrans, h",po::value<double>(&k),"Set heattransfer coefficent, standart 1.0")
                 ;
 
     po::variables_map vm;
@@ -47,9 +74,9 @@ int main(int argc, char* argv[])
 
     //Setting the heat in the left corner
 
-    for (size_t i = 1; i <= 6; ++i)
+    for (size_t i = 1; i <= 1; ++i)
     {
-    	for (size_t j = 1; j <= 6; ++j)
+    	for (size_t j = 1; j <= 1; ++j)
     	{
     		array[i][j]=1.0;
     		array_new[i][j]=1.0;
@@ -59,15 +86,17 @@ int main(int argc, char* argv[])
 	for (int i = 1; i <= iteration; ++i)
 	{
 	    std::swap(array,array_new);
-	    for (int i = 1; i <= 8; ++i)
+
+	    for (int i = 0; i <= 9; ++i)
 	    {
-	    	for (int j = 1; j <= 8; ++j)
+	    	for (int j = 0; j <= 9; ++j)
 	    	{
 
-	    		double dtheta = ((array[i-1][j] + array[i+1][j] - 2*array[i][j])/
-	    					dx) + ((array[i][j-1] + array[i][j+1] - 
-	    					2*array[i][j])/1.0);
-	    		array_new[i][j] = array[i][j] + k*dtheta*dt;
+	    		/*double dtheta = ((array[i-1][j] + array[i+1][j] - 2*array[i][j])/
+	    					dx*dx) + ((array[i][j-1] + array[i][j+1] - 
+	    					2*array[i][j])/dy*dy);
+	    		*/
+	    		array_new[i][j] = array[i][j] + k*computeNewNode(array,i,j)*dt;
 
 	    	}
 	    }
